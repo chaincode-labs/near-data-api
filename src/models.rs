316c9945543471706893bb6daed6b16f002aa516
chaincode-labs <dev@chaincode-labs.org>
2022-05-30 21:30:26 +0800

    xx

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use tokio_pg_mapper_derive::PostgresMapper;
 
 #[derive(Deserialize, PostgresMapper, Serialize)]
-#[pg_mapper(table = "assets__fungible_token_events")] // singular 'user' is a keyword..
+#[pg_mapper(table = "assets__fungible_token_events")]
 pub struct FTRank {
     pub token_id: String,
     pub transaction_count: i64,
