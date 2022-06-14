e25c278444fc69f21f7840747d4d621607e4b307
chaincode-labs <dev@chaincode-labs.org>
2022-06-14 11:56:24 +0800

    use generic for rpc

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,10 +1,10 @@
-use crate::errors::TaskError;
 use curl::easy::{Easy, List};
 use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
 use near_sdk::json_types::U128;
 use near_sdk::serde_json::json;
 use serde::{Deserialize};
 use std::io::Read;
+use serde::de::DeserializeOwned;
 
 #[derive(Deserialize)]
 struct BinData {
@@ -18,7 +18,7 @@ struct JsonData {
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn request_json_rpc_with_body(body: &[u8]) -> Result<Vec<u8>> {
+fn request_json_rpc_with_body<D: DeserializeOwned >(body: &[u8]) -> Result<D> {
     let mut list = List::new();
     list.append("Content-Type: application/json")?;
     let mut easy = Easy::new();
@@ -29,13 +29,8 @@ fn request_json_rpc_with_body(body: &[u8]) -> Result<Vec<u8>> {
 
     let mut data = Vec::new();
     {
-        // Create transfer in separate scope ...
         let mut transfer = easy.transfer();
-
-        // Request body
         transfer.read_function(|buf| Ok(body.as_ref().read(buf).unwrap_or(0)))?;
-
-        // Response body
         transfer.write_function(|new_data| {
             data.extend_from_slice(new_data);
             Ok(new_data.len())
@@ -46,8 +41,8 @@ fn request_json_rpc_with_body(body: &[u8]) -> Result<Vec<u8>> {
 
     println!("{}", easy.response_code()?);
     println!("Received {} bytes", data.len());
-
-    Ok(data)
+    let v = serde_json::from_slice::<JsonData>(data.as_slice())?;
+    Ok(serde_json::from_slice::<D>(v.result.result.as_slice())?)
 }
 
 pub fn get_fungible_token_metadata(id: &String) -> Result<FungibleTokenMetadata> {
@@ -66,17 +61,7 @@ pub fn get_fungible_token_metadata(id: &String) -> Result<FungibleTokenMetadata>
 
     let str = json.to_string();
     let body = str.as_bytes();
-    let data = request_json_rpc_with_body(body)?;
-    if !data.is_empty() {
-        if let Ok(json_value) = serde_json::from_slice::<JsonData>(data.as_slice()) {
-            if let Ok(ft) =
-                serde_json::from_slice::<FungibleTokenMetadata>(json_value.result.result.as_slice())
-            {
-                return Ok(ft);
-            }
-        }
-    }
-    return Err(TaskError::FungibleTokenMetadataNotFound.into());
+    request_json_rpc_with_body::<FungibleTokenMetadata>(body)
 }
 
 pub fn get_fungible_token_total_supply(id: &String) -> Result<U128> {
@@ -95,13 +80,5 @@ pub fn get_fungible_token_total_supply(id: &String) -> Result<U128> {
 
     let str = json.to_string();
     let body = str.as_bytes();
-    let data = request_json_rpc_with_body(body)?;
-    if !data.is_empty() {
-        if let Ok(json_value) = serde_json::from_slice::<JsonData>(data.as_slice()) {
-            if let Ok(b) = serde_json::from_slice::<U128>(json_value.result.result.as_slice()) {
-                return Ok(b);
-            }
-        }
-    }
-    return Err(TaskError::FungibleTokenTotalSupplyNotFound.into());
+    request_json_rpc_with_body::<U128>(body)
 }
